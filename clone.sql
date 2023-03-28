6c192ac9c459fc9fc919c8f14b8be836a9879c06
Kewin Titus <kewintitus@gmail.com>
2023-03-28 21:58:42 +0530

    users table created

--- clone.sql
+++ clone.sql
@@ -1,4 +1,11 @@
--- select now();
+create database instagram;
 
-use salaries;
-select * from employees;
+use instagram;
+
+create table users(
+	id int auto_increment primary key,
+    username varchar(255) unique not null,
+    created_at timestamp default now()	
+);
+
+desc users;
