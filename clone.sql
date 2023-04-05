d0ee8b083ef93fb8cb2b4333f46a7a08b8c07b8f
Kewin Titus <kewintitus@gmail.com>
2023-04-05 19:57:18 +0530

    added new tables

--- clone.sql
+++ clone.sql
@@ -9,3 +9,39 @@ create table users(
 );
 
 desc users;
+
+create table photos(
+	id integer auto_increment primary key, 
+    image_url varchar(255) not null,
+    user_id integer not null,
+    created_at timestamp default now(),
+    foreign key(user_id) references users(id)
+);
+
+create table comments(
+	id int auto_increment primary key,
+    comment_text varchar(255) not null,
+    user_id integer not null,
+    photo_id integer not null,
+    created_at timestamp default now(),
+    foreign key(user_id) references users(id),
+    foreign key(photo_id) references photos(id )
+);
+
+create table likes (
+	user_id integer not null,  
+    photo_id integer not null,
+    created_at timestamp default now(),
+    foreign key(user_id) references users(id),
+    foreign key (photo_id ) references photos(id),
+    primary key(user_id, photo_id)
+);
+
+create table follows (
+	follower_id integer not null,
+    followee_id integer not null,
+    created_at timestamp default now(),
+    foreign key (follower_id) references users(id),
+    foreign key (followee_id) references users(id),
+    primary key(follower_id, followee_id)
+);
