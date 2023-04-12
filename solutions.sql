1675c4ebd1ed66db6ee69509dcb5a8b5c2e4c97f
Kewin Titus <kewintitus@gmail.com>
2023-04-12 21:23:28 +0530

    query fix

--- solutions.sql
+++ solutions.sql
@@ -1,8 +1,5 @@
 
 -- selecting 5 oldest users
-SELECT 
-    *, TIMESTAMPDIFF(YEAR, created_at, NOW()) AS duration
-FROM
-    users
-WHERE
-    TIMESTAMPDIFF(YEAR, created_at, NOW()) > 3 order by created_at  limit 5;
+SELECT * FROM users
+ORDER BY created_at
+LIMIT 5;
